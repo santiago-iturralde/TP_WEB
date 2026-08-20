e5b7992e9fef02fb88a6c6f26f4f596100040451
Santiago Iturralde <siturralde@alumnos.exa.unicen.edu.ar>
2026-08-20 22:26:40 +0000

    Correcciones finales servidor TP1 y README

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module TP_WEB
 
-go 1.23
+go 1.22.2
--- main.go
+++ main.go
@@ -1,46 +1,28 @@
 package main
 
-
 import (
-   "fmt"
-   "net/http"
+	"fmt"
+	"net/http"
 )
 
-
 func main() {
-   //defino el contenido del HTML
-   htmlContent := `<!DOCTYPE html>
-   <html> 
-         <head> <title>Hola mundo </title> </head>              
-         <body> <h1>!SERVIDOR FUNCIONANDO</h1> </body>
-   </html>
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+		
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		
+		http.ServeFile(w, r, "static/index.html")
+	})
 
-   `
+	port := ":8080"
+	fmt.Printf("Servidor escuchando en http://localhost%s\n", port)
 
-   //defino el manejador "HANDLER" de la ruta "/"
-   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "text/html, charset=utf-8")
-    fmt.Fprint(w, htmlContent)//escribo el contenido del HTML en la rta
-   })
-   //Defino el puerto en el que se ejecutará el servidor
-   port := ":8080"
-   fmt.Printf("Servidor escuchando en http://localhost%s\n", port)
-
-
-   err := http.ListenAndServe(port, nil)
-   if err != nil {
-       fmt.Printf("Error al iniciar el servidor: %s\n", err)
-   }
-
-
-//defino el manejador "HANDLER" de la ruta "/"
-   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-       if r.URL.Path != "/" {
-           http.NotFound(w, r) 
-           return
-       }
-       w.Header().Set("Content-Type", "text/html, charset=utf-8")
-       fmt.Fprint(w, htmlContent) 
-   })
-
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		fmt.Printf("Error al iniciar el servidor: %s\n", err)
+	}
 }
+
